b46affefc36eac439ab88e5227b918de22e0877a
David <davidrlutrick@gmail.com>
2022-05-28 20:46:39 -0700

    added a LoggedIn class to control if the user is logged in or not

---
+++ src/application/LoggedIn.java
@@ -0,0 +1,16 @@
+package application;
+
+public class LoggedIn {
+    private boolean loggedIn;
+    public LoggedIn(boolean loggedIn) {
+        this.loggedIn = loggedIn;
+    }
+
+    public void setLoggedIn(boolean loggedIn) {
+        this.loggedIn = loggedIn;
+    }
+
+    public boolean getLoggedIn() {
+        return loggedIn;
+    }
+}
--- src/application/Main.java
+++ src/application/Main.java
@@ -1,10 +1,14 @@
 package application;
 
-import application.userInterface.MainMenu;
-
 public class Main {
     public static void main(String[] args) {
-        MainMenu mainMenu = new MainMenu();
-        System.out.println(mainMenu.displayMenu());
+        LoggedIn loggedIn = new LoggedIn(true);
+        applicationStart(loggedIn.getLoggedIn());
+    }
+
+    public static void applicationStart(boolean loggedIn) {
+        while(loggedIn == true) {
+            
+        }
     }
 }
