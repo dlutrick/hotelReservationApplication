5ab40181aeb537c9d58b83f116cb18262c7b13ce
David <davidrlutrick@gmail.com>
2022-05-28 21:11:18 -0700

    added main menu logic dummy code to test process. Logout feature success.

--- src/application/controller/Application.java
+++ src/application/controller/Application.java
@@ -1,9 +1,32 @@
 package application.controller;
 
+import application.userInterface.MainMenu;
+
+import java.util.Scanner;
+
 public class Application {
+    MainMenu mainMenu = new MainMenu();
+    Scanner scanner = new Scanner(System.in);
     public void start(boolean loggedIn) {
         while(loggedIn) {
-
+            System.out.println(mainMenu.displayMenu());
+            int selection = scanner.nextInt();
+            if(selection == 1) {
+                System.out.println("1");
+            } else if(selection == 2) {
+                System.out.println("2");
+            } else if(selection == 3) {
+                System.out.println("3");
+            } else if(selection == 4) {
+                System.out.println("4");
+            } else if(selection == 5) {
+                System.out.println("5");
+            } else if(selection == 6) {
+                System.out.println("Thank you for using our application! See you soon!");
+                loggedIn = false;
+            } else {
+                System.out.println("----Please select a valid option.----");
+            }
         }
     }
 }
