12b5c1b1e5700a50259db2716869bc76464f530e
David <davidrlutrick@gmail.com>
2022-05-28 19:55:22 -0700

    added customer package along with the Customer.java class

---
+++ src/application/customer/Customer.java
@@ -0,0 +1,37 @@
+package application.customer;
+
+/**
+ * The class for each customer created
+ *
+ * @author davidlutrick
+ */
+public class Customer {
+    private String name;
+    private String email;
+
+    public Customer(String name, String email) {
+        this.name = name;
+        this.email = email;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    @Override
+    public String toString() {
+        return "Name: " + name + "\nEmail: " + email;
+    }
+}
