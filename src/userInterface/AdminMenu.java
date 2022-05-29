3ec65e8b12926405e1bedf5db9f4db09452682d2
David <davidrlutrick@gmail.com>
2022-05-28 18:50:10 -0700

    added private fields, constructor to add the values to each, and the getter methods for each in AdminMenu

--- src/userInterface/AdminMenu.java
+++ src/userInterface/AdminMenu.java
@@ -1,4 +1,40 @@
 package userInterface;
 
+/**
+ * Admin menu class to display the menu a customer will see when booting up the app
+ *
+ * @author davidlutrick
+ */
 public class AdminMenu {
+    private String createAccount;
+    private String searchRooms;
+    private String bookRoom;
+    private String viewRoom;
+
+    AdminMenu() {
+        createAccount = "Create an Account";
+        searchRooms = "Search available rooms";
+        bookRoom = "Book a room";
+        viewRoom = "View a room";
+    }
+
+    /**
+     * Gets the data from AminMenu to display
+     * @return gives access to private fields
+     */
+    public String getCreateAccount() {
+        return createAccount;
+    }
+
+    public String getSearchRooms() {
+        return searchRooms;
+    }
+
+    public String getBookRoom() {
+        return bookRoom;
+    }
+
+    public String getViewRoom() {
+        return viewRoom;
+    }
 }
