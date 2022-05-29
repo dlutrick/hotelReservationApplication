efc39b884e8f9d9888ade2543c2f7e274ff4eecd
David <davidrlutrick@gmail.com>
2022-05-29 00:05:18 -0700

    added the admin menu loop and the back to main menu feature. It also corrects if an invalid selection is used

--- src/application/controller/Application.java
+++ src/application/controller/Application.java
@@ -1,22 +1,37 @@
 package application.controller;
 
 import application.models.customer.Customer;
+import application.userInterface.AdminMenu;
 import application.userInterface.MainMenu;
 
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
 
+/**
+ * The class that contains all of the business logic to control the loop
+ *
+ * @author davidlutrick
+ */
 public class Application {
     MainMenu mainMenu = new MainMenu();
+    AdminMenu adminMenu = new AdminMenu();
     Scanner scanner = new Scanner(System.in);
+
+    /**
+     * Application loop that runs the cli if the user is currently logged in
+     * @param loggedIn boolean value representing the customer being logged in or not
+     */
     public void start(boolean loggedIn) {
+        // Key value pair data structure containing all of the customers that sign up
         Map<String, Customer> customers = new HashMap<>();
         Customer customer;
+        boolean isAdmin = false;
         while(loggedIn) {
             System.out.println("------Welcome valued customer!------");
             System.out.println(mainMenu.displayMenu());
             int selection = scanner.nextInt();
+            // Prompts the user to enter their name and email to create an account
             if(selection == 1) {
                 System.out.println("Please enter your name: ");
                 String name = scanner.next();
@@ -32,12 +47,31 @@ public class Application {
             } else if(selection == 4) {
                 System.out.println("4");
             } else if(selection == 5) {
-                System.out.println("5");
+                isAdmin = true;
+                while(isAdmin) {
+                    System.out.println("------Welcome admin------");
+                    System.out.println(adminMenu.displayMenu());
+                    int adminSelection = scanner.nextInt();
+                    scanner.nextLine();
+                    if(adminSelection == 1) {
+                        
+                    } else if(adminSelection == 2) {
+                        System.out.println("2");
+                    } else if(adminSelection == 3) {
+                        System.out.println("2");
+                    } else if(adminSelection == 4) {
+                        System.out.println("2");
+                    } else if(adminSelection == 5) {
+                        isAdmin = false;
+                    } else {
+                        System.out.println("------Please select a valid option.------");
+                    }
+                }
             } else if(selection == 6) {
                 System.out.println("Thank you for using our application! See you soon!");
                 loggedIn = false;
             } else {
-                System.out.println("----Please select a valid option.----");
+                System.out.println("------Please select a valid option.------");
             }
         }
     }
