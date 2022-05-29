65c7e91fbe08ecd4fc72bfbc5bf483f49a2f9e3b
David <davidrlutrick@gmail.com>
2022-05-29 01:30:50 -0700

    added CustomerService methods for addCustomer, getCustomer and getAllCustomers

--- src/application/Tester.java
+++ src/application/Tester.java
@@ -1,10 +1,17 @@
 package application;
 
 import application.model.customer.Customer;
+import application.service.CustomerService;
 
 public class Tester {
     public static void main(String[] args) {
-        Customer customer = new Customer("first", "second", "email");
-        System.out.println(customer);
+        CustomerService cs = new CustomerService();
+        cs.addCustomer("j@gmail.com", "Joe", "Spaz");
+        cs.addCustomer("k@gmail.com", "kole", "Spaz");
+        cs.addCustomer("l@gmail.com", "lannie", "Spaz");
+
+        System.out.println(cs.getCustomer("k@gmail.com"));
+
+        //System.out.println(cs.getAllCustomers());
     }
 }
--- src/application/service/CustomerService.java
+++ src/application/service/CustomerService.java
@@ -1,4 +1,24 @@
 package application.service;
 
+import application.model.customer.Customer;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
 public class CustomerService {
+    private Customer customer;
+    private Map<String, Customer> customers = new HashMap<>();
+    public void addCustomer(String email, String firstName, String lastName) {
+        customer = new Customer(firstName, lastName, email);
+        customers.put(customer.getEmail(), customer);
+    }
+
+    public Customer getCustomer(String customerEmail) {
+        return customers.get(customerEmail);
+    }
+
+    public Collection<Customer> getAllCustomers() {
+        return customers.values();
+    }
 }
