86795eb65fc677128d765efde0f1365f5509eb48
David <davidrlutrick@gmail.com>
2022-05-28 20:37:52 -0700

    added main menu option to AdminMenu

--- src/application/userInterface/AdminMenu.java
+++ src/application/userInterface/AdminMenu.java
@@ -8,15 +8,17 @@ public class AdminMenu {
     private String displayAllRooms;
     private String displayAllHotelReservations;
     private String addRoom;
+    private String mainMenu;
 
     public AdminMenu() {
         displayAccounts = "1) Display all accounts";
         displayAllRooms = "2) Display all rooms";
         displayAllHotelReservations = "3) Display all reservations";
         addRoom = "4) Add a room";
+        mainMenu = "5) Main menu";
     }
 
     public String displayMenu() {
-        return displayAccounts + "\n" + displayAllRooms + "\n" + displayAllHotelReservations + "\n" + addRoom;
+        return displayAccounts + "\n" + displayAllRooms + "\n" + displayAllHotelReservations + "\n" + addRoom + "\n" + mainMenu;
     }
 }
