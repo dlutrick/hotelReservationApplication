4aa37908d574741827ed318eddd16b9f0c749211
David <davidrlutrick@gmail.com>
2022-05-28 23:43:35 -0700

    added logic for create an account in the application loop

--- src/application/controller/Application.java
+++ src/application/controller/Application.java
@@ -1,18 +1,30 @@
 package application.controller;
 
+import application.models.customer.Customer;
 import application.userInterface.MainMenu;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 public class Application {
     MainMenu mainMenu = new MainMenu();
     Scanner scanner = new Scanner(System.in);
     public void start(boolean loggedIn) {
+        Map<String, Customer> customers = new HashMap<>();
+        Customer customer;
         while(loggedIn) {
+            System.out.println("------Welcome valued customer!------");
             System.out.println(mainMenu.displayMenu());
             int selection = scanner.nextInt();
             if(selection == 1) {
-                System.out.println("1");
+                System.out.println("Please enter your name: ");
+                String name = scanner.next();
+                System.out.println("Please enter your email: ");
+                String email = scanner.next();
+                customer = new Customer(name, email);
+                customers.put(customer.getEmail(), customer);
+                System.out.println(customer.toString());
             } else if(selection == 2) {
                 System.out.println("2");
             } else if(selection == 3) {
